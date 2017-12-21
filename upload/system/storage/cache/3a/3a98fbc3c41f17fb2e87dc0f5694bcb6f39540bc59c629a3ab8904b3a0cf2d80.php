5840bf8b1400b88ab35cce921c7e41572a2d61c3
Puneet <puneetkaka121@gmail.com>
2017-12-21 20:58:58 +0530

    Dj_Outlet

---
+++ upload/system/storage/cache/3a/3a98fbc3c41f17fb2e87dc0f5694bcb6f39540bc59c629a3ab8904b3a0cf2d80.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_e331245ae6a19e643c3d3ce4bb0cc0e92b6a50b7d54b6b31aab87c1ecc4c1e23 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
